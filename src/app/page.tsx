02460d0abcccac975da432ffdfa37885f1f52bc8
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-09-27 20:10:17 +0600

    add Lost in the Music section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeturedSection from "@/components/FeturedSection";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <div>
         <HeroSection />
+        <FeturedSection />
       </div>
     </main>
   );
